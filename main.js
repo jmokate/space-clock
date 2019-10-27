d89f82ec5e70bfae74178e987ef22ad4d22bbbe8
jmokate <jmokate@gmail.com>
2019-10-27 08:43:56 -0500

    updated index and js file

--- main.js
+++ main.js
@@ -1,106 +1,56 @@
+function updateClock() {
+  var today = new Date();
 
-//DATE//
-var today = new Date();
+  var day = today.getDay();
+  var dayNum = today.getDate();
+  var month = today.getMonth();
+  var year = today.getFullYear();
 
-var day = today.getDate();
-var month = today.getMonth();
-var year = today.getFullYear();
+  var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
 
-if (month = 9){
-  month = 'October';
+  var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  var time = today.getHours() + ':' + 
+  today.getMinutes() + ':' + 
+  today.getSeconds();
+
+  var currentDate = document.getElementById('day-of-week');
+  currentDate.innerText = dayArray[day] + ', ' + monthArray[month] + ' ' + + dayNum;
+
+  var currentHour = document.getElementById('hours');
+  currentHour.innerText = today.getHours();
+
+  var currentMin = document.getElementById('minutes');
+  currentMin.innerText = today.getMinutes();
+
+  var currentSec = document.getElementById('seconds');
+  currentSec.innerText = today.getSeconds();
+
+  if (today.getSeconds() < 10) {
+    currentSec.innerText = '0' + today.getSeconds();
+  }
+
+  if (today.getMinutes() < 10) {
+    currentMin.innerText = '0' + today.getMinutes();
+  }
+
+  if (today.getHours() < 10) {
+    currentHour.innerText = '0' + today.getHours();
+  }
+
+
+
+}
+
+function amPm() {
+  var x = document.getElementById('hours').value;
+  if (x.innerText === currentHour.value) {
+    x.innerText = currentHour - 12;
+  } else {
+    x.innerText = currentHour;
+  }
 }
 
 
 
-//TIME//
-var totalHours = today.getHours();
-var time = today.getHours() + ':' + 
-today.getMinutes() + ':' + 
-today.getSeconds();
-
-
-
-//AM - PM OPTION
-//var currentTime = new Date().toLocaleTimeString();
-
-
-//START HOURS TICK
-var hrs = document.getElementById('hours');
-var hrsTick = today.getHours();
-hrs.innerText = hrsTick;
-function hrsInc() {
-  hrsTick++;
-  hrs.innerText = hrsTick;
-
-}
-
-// START MINUTES TICK
-var min = document.getElementById('minutes');
-var minTick = today.getMinutes();
-min.innerText = minTick;
-function minInc() {
-  minTick++;
-  min.innerText = minTick;
-
- // if (minTick >= 60) {
- //   minTick = 0;
- // }
-  
-}  
-// END MINUTES
-
-// START SECONDS TICK //
-var sec = document.getElementById('seconds');
-var tick = today.getSeconds();
-sec.innerText = tick;
-
-// SECONDS AND MINUTES CHANGE
-function increment(){
-  tick++;
-  sec.innerText = tick;
-
-
-  if (tick >= 60) {
-    tick = 0;
-    sec.innerText = tick;
-    minInc();
-    min.innerText = minTick;
-  }
-  if (tick < 10){
-    sec.innerText = '0' + tick;
- }
-  
-  //MINUTES AND HOURS CHANGE
-  if (minTick >= 60) {
-    minTick = 0;
-    min.innerText = minTick;
-    hrsInc();
-    hrs.innerText = hrsTick;
-  }
-  if (hrsTick >= 24){
-    hrsTick = 0;
-    hrs.innerText = hrsTick;
-  }
-
-  if (minTick < 10) { 
-    min.innerText = '0' + minTick;
-  }
-  
-  if (hrsTick < 10){
-    hrs.innerText = '0' + hrsTick;
-  }
-
-}
-setInterval(increment, 1000);
-
-
-
-
-
-
-//CONSOLE LOG
-
-console.log(time);
-console.log(today);
-console.log(month);
-//console.log(currentTime);
+setInterval(updateClock, 1000);
