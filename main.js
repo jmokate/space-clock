c35a7b6976883ccf9ecb3a4e4d0959ac9e153f2c
jmokate <jmokate@gmail.com>
2019-11-01 15:41:54 -0500

    changed variables to const and moved arrays to global scope

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
+const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
+const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 function updateClock() {
 
-  let today = new Date();
+  const today = new Date();
 
-  let day = today.getDay();
-  let dayNum = today.getDate();
-  let month = today.getMonth();
-  
+  const day = today.getDay();
+  const dayNum = today.getDate();
+  const month = today.getMonth();
 
-  let monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
-
-  let dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-  let currentDate = document.getElementById('day-of-week');
+  const currentDate = document.getElementById('day-of-week');
   currentDate.innerText = dayArray[day] + ', ' + monthArray[month] + ' ' + + dayNum;
 
-  let currentHour = document.getElementById('hours');
+  const currentHour = document.getElementById('hours');
   currentHour.innerText = today.getHours();
 
-  let currentMin = document.getElementById('minutes');
+  const currentMin = document.getElementById('minutes');
   currentMin.innerText = today.getMinutes();
 
-  let currentSec = document.getElementById('seconds');
+  const currentSec = document.getElementById('seconds');
   currentSec.innerText = today.getSeconds();
 
   if (today.getSeconds() < 10) {
